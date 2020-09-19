a5513e3047ae740ff4b711d7ea7bf060e76775ba
tkdlxm457 <tkdlxm457@naver.com>
2020-09-19 21:27:55 +0900

    codeup1093~1095

---
+++ 1093~1095기초-1차원배열/1094-[기초-1차원배열] 이상한 출석 번호 부르기2.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+int main()
+{
+    
+    int num, arr[10000];
+    scanf("%d",&num);
+ 
+    for(int i=0; i<num; i++){
+        scanf("%d", &arr[i]);
+    }
+    for(int i=num-1; i>=0; i--){
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
